ccdca084971cd908347eeaade23f1f18911fafdf
Yevhen <leshchenkoyevhen@gmail.com>
2023-05-07 16:48:09 -0400

    first commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+let age: number = 50;
+let names: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback = (a: number) => {
+  return 100 + a;
+};
+
+let anything: any = -20;
+anything = "Text";
+anything = {};
+
+// let some: unknown;
+// some = "Text";
+
+// let str: string;
+
+// str = some;
