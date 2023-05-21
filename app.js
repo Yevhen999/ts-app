ed8f8321821c8a5b989018d1d523719ea1e4997e
Yevhen <leshchenkoyevhen@gmail.com>
2023-05-22 07:33:47 +0800

    comp 2

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var age = 50;
-var names = "Max";
+var nam = "Max";
 var toggle = true;
 var empty = null;
 var notInitialize = undefined;
-var callback = function (a) {
-    return 100 + a;
-};
+// let callback = (a: number) => number;
+// callback = (a) => {return 100 + a};
 var anything = -20;
 anything = "Text";
 anything = {};
@@ -21,8 +20,21 @@ var Status;
     Status[Status["LOADING"] = 0] = "LOADING";
     Status[Status["READY"] = 1] = "READY";
 })(Status || (Status = {}));
-var a = "";
-var b = "enable";
+var page = {
+    load: Status.READY,
+};
+if (page.load === Status.LOADING) {
+    console.log("The page is loading");
+}
+if (page.load === Status.READY) {
+    console.log("The page has been loaded");
+}
+var a;
+a = 10;
+a = "hello";
+var b;
+b = "disable";
+b = "enable";
 function showMessage(message) {
     console.log(message);
 }
@@ -32,3 +44,19 @@ function calc(num1, num2) {
 function customError() {
     throw new Error("Error");
 }
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
 let age: number = 50;
-let names: string = "Max";
+let nam: string = "Max";
 let toggle: boolean = true;
 let empty: null = null;
 let notInitialize: undefined = undefined;
-let callback = (a: number) => {
-  return 100 + a;
-};
+// let callback = (a: number) => number;
+
+// callback = (a) => {return 100 + a};
 
 let anything: any = -20;
 anything = "Text";
@@ -27,11 +27,27 @@ enum Status {
   READY,
 }
 
+const page = {
+  load: Status.READY,
+};
+
+if (page.load === Status.LOADING) {
+  console.log("The page is loading");
+}
+
+if (page.load === Status.READY) {
+  console.log("The page has been loaded");
+}
+
 let a: string | number;
+
+a = 10;
+a = "hello";
 
 let b: "enable" | "disable";
 
 b = "disable";
+b = "enable";
 
 function showMessage(message: string): void {
   console.log(message);
@@ -45,20 +61,31 @@ function customError(): never {
   throw new Error("Error");
 }
 
-// const page1 = {
-//   title: "The awesome page",
-//   likes: 100,
-//   accounts: ["Max", "Anton", "Nikita"],
-//   status: "open",
-//   details: {
-//     createAt: "2021-01-01",
-//     updateAt: "2021-05-01",
-//   },
-// };
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
 
-// const page2 = {
-//   title: "Python or Js",
-//   likes: 5,
-//   accounts: ["Alex"],
-//   status: "close",
-// };
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
