03af7040544ed14db7f7479478b46e1c8b99583a
Yevhen <leshchenkoyevhen@gmail.com>
2023-07-02 17:52:58 -0400

    edit inputEmail

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
 const input = document.getElementById("inputEmail");
+
+if (input) {
+  (input as HTMLInputElement).value;
+  const newInput = input as HTMLInputElement;
+  console.log(newInput.value);
+}
