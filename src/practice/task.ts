01ddee78d0ad2b896139b1b8ac7f060e8fe782e4
Yevhen <leshchenkoyevhen@gmail.com>
2023-07-03 19:33:36 -0400

    prct two

--- src/practice/task.ts
+++ src/practice/task.ts
@@ -7,3 +7,23 @@ function getPromise(): Promise<Array<string | number>> {
 getPromise().then((data): void => {
   console.log(data);
 });
+// --------------------------------prct two--------------
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
