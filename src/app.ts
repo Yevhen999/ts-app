6c56761827b103753ede96963ad99a940175a108
Yevhen <leshchenkoyevhen@gmail.com>
2023-07-03 18:48:48 -0400

    keyof

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function extractValue<T extends object, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
 }
+
+const field = extractValue({ name: "Yevhen" }, "name");
